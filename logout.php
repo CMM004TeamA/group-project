4e28c3a1bb6521e5b257a03a420ff73aad91b8c2
AyaUchiv <uchivaya@gmail.com>
2025-04-06 01:13:09 +0100

    Update logout.php
    added pop up function when admin logs out

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@ session_start();
 session_unset();
 session_destroy();
 
-header("location: index.php");
+echo "<script type='text/javascript'>alert('Log out');</script>";
+echo "<script>window.location.href = 'index.php';</script>";
 exit;
 ?>
